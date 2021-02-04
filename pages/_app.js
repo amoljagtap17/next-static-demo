3060b1d7b3feceb869787ec050733f5aa913f1f8
Amol Jagtap <ajfsdeveloper@gmail.com>
2021-02-04 21:36:29 +0530

    Add Layout

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,25 @@
 import 'antd/dist/antd.min.css'
+import { Layout } from 'antd'
 import '../styles/globals.css'
 
+const Container = ({ children }) => {
+  const { Header, Footer, Content } = Layout
+
+  return (
+    <Layout>
+      <Header>Header</Header>
+      <Content>{children}</Content>
+      <Footer>Footer</Footer>
+    </Layout>
+  )
+}
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Container>
+      <Component {...pageProps} />
+    </Container>
+  )
 }
 
 export default MyApp
